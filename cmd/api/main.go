477b3253d72b0200199b1fd39636660570eb0c27
m0hh <lordegy53@gmail.com>
2024-03-04 17:03:12 +0200

    added CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -65,6 +69,10 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "0eb39a152fddce", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "67433d292d5bda", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
